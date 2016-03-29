a727f52e1a0d584cce5040e03d744e3f02a3fe3e
guillermo.cattaneo@gmail.com <guillermo.cattaneo@gmail.com>
2016-03-29 19:33:23 -0300

    Metodo post

---
+++ mostrar.php
@@ -0,0 +1,20 @@
+<h1> Llego </h1>
+
+<?php 
+
+//echo "lalala";
+echo "<h1> Llego </h1>";
+//Super variables "$_POST" o "$_GET" son arrays, vemos sus contenidos con arrays
+
+echo "Post <br>";
+var_dump($_POST);
+echo "<br><br>";
+
+echo "Get <br>";
+var_dump($_GET);	//tiene todo publico, se ve en la barra de direccion.
+echo "<br><br>";
+
+
+
+
+ ?>
