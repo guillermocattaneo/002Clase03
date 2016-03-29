9841534ff8f7af0e32c2282df8caa51d9fc25e25
guillermo.cattaneo@gmail.com <guillermo.cattaneo@gmail.com>
2016-03-29 19:52:24 -0300

    Agregado 2 paginas para meterlas en un if.

--- mostrar.php
+++ mostrar.php
@@ -1,8 +1,10 @@
+<!--
 <h1> Llego </h1>
-
+-->
 <?php 
 
 //echo "lalala";
+
 echo "<h1> Llego </h1>";
 //Super variables "$_POST" o "$_GET" son arrays, vemos sus contenidos con arrays
 
@@ -15,6 +17,18 @@ var_dump($_GET);	//tiene todo publico, se ve en la barra de direccion.
 echo "<br><br>";
 
 
+$nombre = $_POST['usuario'];
+echo "Benvenutto: " . $nombre;
+$contrasena = $_POST['clave'];
+echo ", tu clave esta segura aca (aunque sea): <h2>" . $contrasena."</h2> ;-)";
 
+if ($nombre=="admin" && $contrasena="1234") {
+	include "bienvenido.html";
+
+}else
+{
+	include "noLogeado.html";
+
+}
 
  ?>
